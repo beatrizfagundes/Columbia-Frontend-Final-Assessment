227da087dab38fdabc235e51dca1586af2ed93be
Beatriz Santana Fagundes Souza de Lima <bs2899@columbia.edu>
2015-05-11 03:00:07 -0400

    missing functionality to accept /about route

---
+++ public/app.js
@@ -0,0 +1,45 @@
+var app = angular.module('FinalExam', []);
+
+app.controller('MainController', function ($scope, Homework) {
+
+    Homework.getHomeworks().then(function(response) {
+        $scope.homeworks = response;
+    });
+
+    $scope.showAssignmentInfo = function (homework) {
+        homework.clicked = !homework.clicked;
+        homework.completed = (homework.status == 'completed');
+    }
+
+});
+
+app.factory('Homework', function ($http) {
+
+    return {
+        getHomeworks: function() {
+            return $http.get('/').then(function () {
+                homeworks = [
+                    {
+                        assignment: 'Test First JavaScript',
+                        url: 'https://github.com/beatrizfagundes/FullstackTestFirst',
+                        status: 'completed'
+                    },
+                    {
+                        assignment: 'Angular Flash Cards - Day 1',
+                        url: 'https://github.com/beatrizfagundes/flashcards-day1',
+                        status: 'completed'
+                    },
+                    {
+                        assignment: 'Angular Flash Cards - Day 2',
+                        url: 'https://github.com/beatrizfagundes/flash-cards',
+                        status: 'completed'
+                    }
+                ];
+
+                return homeworks;
+
+            });
+        }
+    };
+
+});
